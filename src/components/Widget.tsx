42e9569d05a1ba91df502779990f6d325b978352
Kélison Rosendo da Rocha <kelisonrosendo@gmail.com>
2022-05-03 18:55:22 -0300

    criação do componente widget form

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,4 +1,5 @@
 import { ChatTeardropDots } from 'phosphor-react';
+import { WidgetForm } from './WidgetForm';
 import { useState } from 'react';
 
 export function Widget() {
@@ -9,8 +10,8 @@ export function Widget() {
   }
 
   return (
-    <div className="absolute bottom-5 right-5">
-      {isWidgetOpen && <p>Hello World!</p>}
+    <div className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
+      {isWidgetOpen && <WidgetForm />}
 
       <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
